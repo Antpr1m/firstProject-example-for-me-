3f6a0b17add7b501c79e0f283115d33039b13b10
Antpr1m <a.primakov87@gmail.com>
2022-05-24 12:09:46 +0300

    Class component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -23,3 +23,42 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users
+    }
+}
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId))
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users))
+        }
+    }
+}
+
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)*/
